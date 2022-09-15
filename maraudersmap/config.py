15651a4f29106948f30f3a06f63432fb8806f739
Mathias Oterhals Myklebust <mathias@oterbust.no>
2022-09-15 10:28:45 +0200

    Add env file support and centralize db dialect fix

--- maraudersmap/config.py
+++ maraudersmap/config.py
@@ -1,12 +1,27 @@
-from pydantic import BaseSettings
+from pydantic import BaseSettings, validator
 
 
 class Settings(BaseSettings):
     DATABASE_CONNECTION_STRING: str | None = None
 
+    @validator("DATABASE_CONNECTION_STRING", pre=True, always=True)
+    def replace_deprecated_db_dialect(cls, v: str | None) -> str | None:
+        if v is None:
+            return v
+        return v.replace(
+            # SQLAlchemy 1.4 removed deprecated 'postgres://',
+            # but fly.io still uses it...
+            "postgres://",
+            "postgresql://",
+            1,
+        )
+
     class Config:
+        env_file = ".env"
+        env_file_encoding = "utf-8"
         fields = {
             "DATABASE_CONNECTION_STRING": {
+                # fly.io injects DATABASE_URL, so we define it as an alias
                 "env": ["DATABASE_CONNECTION_STRING", "DATABASE_URL"]
             }
         }
